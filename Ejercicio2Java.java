60a32d08a0a13af761b32f89065590b6b9da5123
anaVale <30755891+anaVale@users.noreply.github.com>
2017-12-09 22:48:14 -0300

    Add files via upload

---
+++ Ejercicio2Java.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio2java;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Ejercicio2Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1=720;
+        int num2=135;
+        int num3=245;
+        System.out.println("La suma de los numeros es:" +(num1+num2+num3));
+        System.out.println("La resta de los numeros es:" +(num1-num2-num3));
+        System.out.println("La multiplicación de los numeros es:" +(num1*num2*num3));
+        System.out.println("Valeria Neer 09-12-2017");
+
+    }  
+ 
+}   
+}

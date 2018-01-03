b51c30ead93d2f6dbe272fbbb405e94bc8f510fd
anaVale <30755891+anaVale@users.noreply.github.com>
2018-01-03 16:08:08 -0300

    Add files via upload

---
+++ Eje1Superficie.java
@@ -0,0 +1,22 @@
+package primerprograma;
+import java.util.*;
+
+public class Eje1Superficie {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      int superficie;
+     
+      Scanner entrada = new Scanner(System.in);
+      System.out.println("Ingrese base: ");
+      int base =entrada.nextInt();
+      System.out.println("Ingrese altura: ");
+      int altura=entrada.nextInt();
+      superficie=base*altura;
+      System.out.println("La superficie de base:" +base +" y altura: " +altura +" es: " +superficie);
+      entrada.close();
+    }
+    
+}

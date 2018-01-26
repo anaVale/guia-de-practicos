a7fd6e55396527e6577720b373c34f6d1d77abc4
anaVale <30755891+anaVale@users.noreply.github.com>
2018-01-25 21:20:10 -0300

    Add files via upload

---
+++ Eje14Hola.java
@@ -0,0 +1,15 @@
+
+package ejes;
+
+
+public class Eje14Hola {
+
+    
+    public static void main(String[] args) {
+      for (int i=1;i<=10;i++){
+          System.out.println("Hola");
+      }
+      
+    }
+    
+}

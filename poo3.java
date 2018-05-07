1e5192a5ed8ac83f6972b4db0f2716598d719e1a
anaVale <30755891+anaVale@users.noreply.github.com>
2018-05-07 00:40:35 -0300

    Add files via upload

---
+++ poo3.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+
+public class poo3 {
+
+   
+    public static void main(String[] args) {
+      int dia, mes, anio; 
+      dia=14;
+      mes=07;
+      anio=2018;
+      /* Scanner entrada = new Scanner(System.in);
+       System.out.print("Ingrese día: ");
+       dia=entrada.nextInt();
+      System.out.print("Ingrese mes: ");
+       mes=entrada.nextInt();
+       System.out.print("Ingrese año: ");
+       anio=entrada.nextInt();*/
+       System.out.println("fecha: " +dia + "-" +mes +"-" +anio);
+       
+    }
+    
+}

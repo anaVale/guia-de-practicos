a2a5c34b8b6d1e258c5823fa51c5f0a83dcd3fd4
anaVale <30755891+anaVale@users.noreply.github.com>
2018-05-25 22:50:52 -0300

    Add files via upload

---
+++ U3PooEje5.java
@@ -0,0 +1,18 @@
+
+public class U3PooEje5 {
+
+    public static void main(String[] args) {
+        int[] A = {1, 3, 5, 7, 9};
+        int[] B = {2, 3, 5, 7, 5};
+        int[] C = new int[5];
+
+        for (int i = 0; i <= 4; i++) {
+            C[i] = A[i] + B[i];
+        }
+        for (int i = 0; i <= 4; i++) {
+            System.out.println(C[i]);
+        }
+
+    }
+
+}
